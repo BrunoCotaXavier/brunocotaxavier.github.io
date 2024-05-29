ab38c59f5d6b6c071635e3f6968b2b981ab81aec
BrunoCotaXavier <brunocota@hotmail.com.br>
2024-05-28 21:22:17 -0300

    add-widget

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     const widgetContainer = document.getElementById('translator-widget');
     
     const startButton = document.createElement('button');
-    startButton.textContent = 'Ativar Captura de Texto';
+    startButton.textContent = 'Ativar Assistente';
     let isCapturing = false;
 
     const outputContainer = document.createElement('div');
@@ -10,16 +10,14 @@
 
     startButton.onclick = function(event) {
         isCapturing = !isCapturing;
-        startButton.textContent = isCapturing ? 'Desativar Captura de Texto' : 'Ativar Captura de Texto';
-        event.stopPropagation(); // Evita que o clique no botão ative a captura de texto
+        startButton.textContent = isCapturing ? 'Desativar Assistente' : 'Ativar Assistente';
+        event.stopPropagation(); 
     };
 
     document.addEventListener('click', function(event) {
         if (isCapturing) {
-            // Verifica se o clique foi dentro do widget para evitar capturar texto do próprio widget
             if (!widgetContainer.contains(event.target)) {
                 const textContent = event.target.innerText || event.target.textContent;
-                outputContainer.textContent = textContent;
                 let ut = new SpeechSynthesisUtterance(textContent);
                 window.speechSynthesis.speak(ut)
             }
